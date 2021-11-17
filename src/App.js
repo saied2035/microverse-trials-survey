11c15948262cb381406387f5bb2cd31f36a2dd24
unknown <saied2421998@gmail.com>
2021-11-17 20:23:11 +0200

    final project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import './App.css';
 import 'react-bootstrap';
-import { Button, Form, Dropdown } from 'react-bootstrap';
+import { Button, Form, Dropdown, InputGroup, FormControl } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
        <h1 id="title">microverse trials survey</h1>
-       <p id="description">this is a survey fpr microverse trials.</p>
-       {/* <form id="survey-form">
-         
-         <input type="email" id="email"/>
-       </form> */}
-
+       <p id="description">this is a survey for microverse trials.</p>
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Name</Form.Label>
+          <Form.Label id="name-label">Name</Form.Label>
           <Form.Control type="text" placeholder="Enter your name" id="name" />
-        
-          <Form.Label>Email address</Form.Label>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label id="email-label">Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" id='email'/>
-        
-          <Form.Label>Number</Form.Label>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label id="number-label">Number</Form.Label>
           <Form.Control type="number" placeholder="Enter number" id='number'/>
         </Form.Group>
         
@@ -30,14 +29,41 @@ function App() {
             Dropdown Button
           </Dropdown.Toggle>
 
-          <Dropdown.Menu>
+          <Dropdown.Menu id="dropdown">
             <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
             <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
             <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
+          <InputGroup className="flex justify-center mb-3">
 
-        <Button variant="primary" type="submit">
+                <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+                <FormControl className="ml1 mw5" value="Something" aria-label="Text input with checkbox" disabled/>
+
+                <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+                <FormControl className="ml1 mw5" value="Something" aria-label="Text input with checkbox" disabled/>
+
+                <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+                <FormControl className="ml1 mw5" value="Something" aria-label="Text input with checkbox" disabled/>                                
+              </InputGroup>
+
+              <InputGroup className="flex justify-center mb-3">
+
+                <InputGroup.Radio aria-label="Radio button for following text input" />
+                <FormControl className="ml1 mw5" value="Something else" 
+                aria-label="Text input with radio button" disabled/>
+
+                <InputGroup.Radio aria-label="Radio button for following text input" />
+                <FormControl  className="ml1 mw5" value="Something else" 
+                aria-label="Text input with radio button" disabled/>
+
+                <InputGroup.Radio aria-label="Radio button for following text input" />
+                <FormControl className="ml1 mw5" value="Something else" 
+                aria-label="ml1 Text input with radio button" disabled/>     
+
+          </InputGroup>
+          <textarea className="" placeholder="your feedback"></textarea>
+        <Button id="submit" variant="primary" type="submit">
           Submit
         </Button>
       </Form>
