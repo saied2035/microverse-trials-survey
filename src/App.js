ae6ce4def24faa3a162268676d42db22b2b4c696
Shaheda Khatoon <khanshaheda2013@gmail.com>
2021-11-17 23:01:42 +0530

    added form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-
 import './App.css';
+import 'react-bootstrap';
+import { Button, Form, Dropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
        <h1 id="title">microverse trials survey</h1>
        <p id="description">this is a survey fpr microverse trials.</p>
-       <form id="survey-form">
-         <input type="text" id="name"/>
+       {/* <form id="survey-form">
+         
          <input type="email" id="email"/>
-       </form>
+       </form> */}
+
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter your name" id="name" />
+        
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" id='email'/>
+        
+          <Form.Label>Number</Form.Label>
+          <Form.Control type="number" placeholder="Enter number" id='number'/>
+        </Form.Group>
+        
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Dropdown Button
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
